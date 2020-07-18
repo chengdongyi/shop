04a5676e09f2b0128d8f9ef06ba3b447efa1dcdd
chengdongyi <cheng.123>
2020-07-18 18:04:05 +0800

    123

---
+++ shop-common/src/main/java/com/example/shop/common/Test.java
@@ -0,0 +1,19 @@
+package com.example.shop.common;
+
+/**
+ * @author chengdongyi
+ * @description:
+ * @date 2020/7/18 17:02
+ */
+public class Test {
+
+    private String aa;
+
+    public String getAa() {
+        return aa;
+    }
+
+    public void setAa(String aa) {
+        this.aa = aa;
+    }
+}
---
+++ shop-server-user/src/main/java/com/example/shop/user/UserApplication.java
@@ -0,0 +1,16 @@
+package com.example.shop.user;
+
+//import com.example.shop.common.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserApplication {
+
+    public static void main(String[] args) {
+
+//        Test test = new Test();
+        SpringApplication.run(UserApplication.class, args);
+    }
+
+}
